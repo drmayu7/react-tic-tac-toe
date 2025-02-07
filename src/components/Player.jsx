da162a8cb5499c80dad4b4d6ddac9b8c2ea237fa
Dr Muhammad Naufal <117550441+drmayu7@users.noreply.github.com>
2025-02-07 17:59:35 +0800

    add isActive props to Player.jsx component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,7 +1,7 @@
 import {useState} from "react";
 
 
-export default function Player({initialName, symbol}) {
+export default function Player({initialName, symbol, isActive}) {
     const [playerName, setPlayerName] = useState(initialName)
     const [isEditing, setIsEditing] = useState(false);
 
@@ -14,15 +14,13 @@ export default function Player({initialName, symbol}) {
     }
 
     let editablePlayerName = <span className='player-name'> {playerName}</span>
-    // let btnText = 'EDIT';
 
     if (isEditing) {
         editablePlayerName = <input type='text' required value={playerName} onChange={handleSaveClick}/>
-        // btnText = 'SAVE';
     }
 
     return (
-        <li>
+        <li className={isActive ? 'active' : undefined}>
             <span className='player'>
                 {editablePlayerName}
                 <span className='player-symbol'>{symbol}</span>
