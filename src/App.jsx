2d1179e184215536a648c834d93d4e23ed6e5d8e
Dr Muhammad Naufal <117550441+drmayu7@users.noreply.github.com>
2025-02-07 18:04:23 +0800

    fix bug 'O' player not highlighted when active

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
             <div id='game-container'>
                 <ol id='players' className='highlight-player'>
                     <Player initialName='Player 1' symbol='X' isActive={activePlayer==='X'}/>
-                    <Player initialName='Player 2' symbol='O' isActive={activePlayer==='0'}/>
+                    <Player initialName='Player 2' symbol='O' isActive={activePlayer==='O'}/>
                 </ol>
                 <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol= {activePlayer} />
             </div>
