f2955a4e70a43f4842db600e3d89b168d32c25a8
Dr Muhammad Naufal <117550441+drmayu7@users.noreply.github.com>
2025-02-07 18:00:14 +0800

    add props to highlight active player

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,16 +6,17 @@ const initialGameBoard = [
     [null, null, null],
 ]; // object to represent the game board
 
-export default function GameBoard() {
+export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
     const [gameBoard, setGameBoard] = useState(initialGameBoard)
 
     function handleSelectSquare(rowIndex, colIndex) {
         setGameBoard((prevGameBoard) => {
             const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])]; //copy the game board - to prevent mutation bugs
-            updatedBoard[rowIndex][colIndex] = 'X';
+            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
             return updatedBoard;
             }
         );
+        onSelectSquare();
     }
 
     return (
