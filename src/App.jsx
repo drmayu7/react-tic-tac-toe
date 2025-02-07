f7226aa463de603188cddc163c4e34c78f8de8ff
Dr Muhammad Naufal <117550441+drmayu7@users.noreply.github.com>
2025-02-07 15:23:09 +0800

    Gameboard components - UI without state

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import Player from "./components/Player.jsx";
+import GameBoard from "./components/GameBoard.jsx";
 
 function App() {
     return (
         <main>
             <div id='game-container'>
                 <ol id='players'>
-                <Player initialName='Player 1' symbol='X' />
-                <Player initialName='Player 2' symbol='O' />
+                    <Player initialName='Player 1' symbol='X'/>
+                    <Player initialName='Player 2' symbol='O'/>
                 </ol>
-
-                GAMEBOARD GAME
+                <GameBoard />
             </div>
 
             LOG
