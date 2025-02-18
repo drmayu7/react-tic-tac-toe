3bd1212b3915edc3d6437c922ce3b201c8d5a82f
Dr Muhammad Naufal <117550441+drmayu7@users.noreply.github.com>
2025-02-18 12:16:24 +0800

    selected button to be unique to one player only

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -33,7 +33,7 @@ export default function GameBoard({ onSelectSquare,turns }) {
                 (row, rowIndex) => <li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, colIndex) => <li key={colIndex}>
-                            <button onClick={ () => onSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button>
+                            <button onClick={ () => onSelectSquare(rowIndex,colIndex)} disabled={playerSymbol !== null }>{playerSymbol}</button>
                         </li>)
                         }
                     </ol>
