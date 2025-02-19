a078955908c62c9e31c5375853e160231f210016
Dr Muhammad Naufal <117550441+drmayu7@users.noreply.github.com>
2025-02-19 12:53:04 +0800

    create player state, without lifting out original state in player component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,15 @@
 import {useState} from "react";
 
 
-export default function Player({initialName, symbol, isActive}) {
+export default function Player({initialName, symbol, isActive, onChangeName}) {
     const [playerName, setPlayerName] = useState(initialName)
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
         setIsEditing((prevState) => !prevState); // toggle the value of isEditing, using !isEditing is not recommended
+        if (isEditing){
+            onChangeName(symbol,playerName)
+        }
     }
 
     function handleSaveClick(event) {
